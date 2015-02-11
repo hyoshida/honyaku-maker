9c68439248fe595a486e77169e4f40b30e5f4df6
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-02-12 00:16:36 +0900

    factory_girl => factory_girl_rails

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,3 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'factory_girl'
-
-class ActiveSupport::TestCase
-  FactoryGirl.find_definitions
-end
